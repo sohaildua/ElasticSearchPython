58c52e29c49d06a1c280a9a7c607cf1370ebbdc9
sdgamer007 <sohaildua38@gmail.com>
2022-10-22 04:49:01 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,39 @@
+
+import os, base64, re, logging
+from elasticsearch import Elasticsearch
+
+# Log transport details (optional):
+logging.basicConfig(level=logging.INFO)
+os.environ['BONSAI_URL'] =  'url'
+# Parse the auth and host from env:
+try:
+    bonsai = os.environ['BONSAI_URL']
+    print(bonsai)
+except Exception as e:
+    print(e)
+
+auth = re.search('https\:\/\/(.*)\@', bonsai).group(1).split(':')
+host = bonsai.replace('https://%s:%s@' % (auth[0], auth[1]), '')
+
+# optional port
+match = re.search('(:\d+)', host)
+if match:
+  p = match.group(0)
+  host = host.replace(p, '')
+  port = int(p.split(':')[1])
+else:
+  port=443
+
+# Connect to cluster over SSL using auth for best security:
+es_header = [{
+ 'host': host,
+ 'port': port,
+ 'use_ssl': True,
+ 'http_auth': (auth[0],auth[1])
+}]
+
+# Instantiate the new Elasticsearch connection:
+es = Elasticsearch(es_header)
+
+# Verify that Python can talk to Bonsai (optional):
+es.ping()
